0a8c3bfb23e0a16d4776215b54170394b2ecfd31
Samuel Cornet <samuel@noukod.com>
2019-01-30 16:09:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
 //Variables defined in the test suite:
 let oldID ++;
-let newID = 1000000000;
-newID = 1000000000 + oldID;
+let newID = 1000000000 + oldID;
 //- currentAge
 
 //You should NOT try to redefine any of the above variables.
