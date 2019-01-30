908da769988f49435bb52fd97a1808332693cc0d
Samuel Cornet <samuel@noukod.com>
2019-01-30 15:34:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/*
-Variables defined in the test suite:
-- oldID
-- currentAge
 
-You should NOT try to redefine any of the above variables.
+//Variables defined in the test suite:
+const oldID = 1;
+newID = oldID + 1000000000;
+//- currentAge
+
+//You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
