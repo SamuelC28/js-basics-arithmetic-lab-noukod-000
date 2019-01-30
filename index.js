7035bad696f9d791112ad27fc75e40d54d72fc99
Samuel Cornet <samuel@noukod.com>
2019-01-30 16:29:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ let newID = 1000000000 + oldID;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.floor(Math.random() * 20) + 1;
 let randomInteger = Math.round(randomNumber);
-let randomUserID = Math.floor(Math.random() * newID + 20) +1); 
+let randomUserID = Math.floor(Math.random(newID) * newID + 20) + 1); 
